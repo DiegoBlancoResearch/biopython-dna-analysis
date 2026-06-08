615603e6ee188f61a0398c4762ae47251db60de4
Diego Blanco <diegoblancob@icloud.com>
2026-06-08 09:36:32 -0600

    Add integrated NCBI and FASTA analysis

--- src/ncbi_and_full_analisis.py
+++ src/ncbi_and_full_analisis.py
@@ -1,26 +1,28 @@
 from Bio import Entrez, SeqIO
-Entrez.email = "diegoblancob2011@gmail.com"
+Entrez.email = "diegoblancob2011@icloud.com"
 genes_list = ["NM_000484.4", "NM_001377265", "KR710721.1"]
+
 
 def fetch_sequence(accession_id):
     try:
         handle = Entrez.efetch(db="nucleotide", id=accession_id, rettype="fasta", retmode="text")
         record = SeqIO.read(handle, "fasta")
         handle.close()
-        print(f"Successfully fetched sequence for {accession_id}")
-        print(f"Sequence ID: {record.id}") 
-        print(f"Sequence length: {len(record.seq)}")
-        return record
+        gen={
+        "Sequence ID": record.id,
+        "Sequence length": len(record.seq),
+        "gc": (record.seq.count('G') + record.seq.count('C')) / len(record.seq) * 100,
+        "sequence": record.seq
+        }
+        return gen
     except Exception as e:
         print(f"Error fetching sequence for {accession_id}: {e}")
         return None
    
     
-def genes_loop():   
+def genes_loop():
     for gene in genes_list:
-        fetch_sequence(gene)
-
+        result = fetch_sequence(gene)
+        print(result)
 
 genes_loop()
-
-
